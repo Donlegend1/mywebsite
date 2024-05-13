d743f2b4c5a18b44367dd2b15cab5131a620a2e4
Shedrack Ogwuche <shedrackogwuche5@gmail.com>
2024-05-13 10:24:03 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,84 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import React, { Fragment } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-
-
-
-
-
-//App
-const App = React.lazy(() => import("./App"));
-const Home = React.lazy(() => import("./pages/Home"));
-const About = React.lazy(() => import("./pages/About"));
-const Services = React.lazy(() => import("./pages/Services"));
-const Projects = React.lazy(() => import("./pages/Projects"));
-const Contact = React.lazy(() => import("./pages/Contact"));
-
-
-
-//Dashboard
-
-
-const Private = ({ children }) => {
-  return children;
-};
-
-const Loaderimg = () => {
-  return (
-    <div id="global-loader">
-      <img
-        src={"/assets/img/about-2.jpg"}
-        className="loader-img"
-        alt="Loader"
-      />
-    </div>
-  );
-};
-
-const Root = () => {
-
-
-  return (
-    <Fragment>
-      <BrowserRouter>
-        <React.Suspense fallback={Loaderimg()}>
-          {/* <AuthProvider> */}
-
-          <Routes>
-            <Route element={<App />}>
-              <Route
-                path={`${process.env.PUBLIC_URL}/`}
-                element={<Home />}
-              />
-
-              <Route
-                path={`${process.env.PUBLIC_URL}/contact`}
-                element={<Contact />}
-              />
-              <Route
-                path={`${process.env.PUBLIC_URL}/about`}
-                element={<About />}
-              />
-              <Route
-                path={`${process.env.PUBLIC_URL}/services`}
-                element={<Services />}
-              />
-              <Route
-                path={`${process.env.PUBLIC_URL}/projects`}
-                element={<Projects />}
-              />
-            </Route>
-          </Routes>
-
-          {/* </AuthProvider>  */}
-        </React.Suspense>
-      </BrowserRouter>
-    </Fragment>
-  );
-};
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Root />);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
